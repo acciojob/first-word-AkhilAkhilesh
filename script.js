495363475829aff02920bedbdee1274fed968b84
AkhilAkhilesh <120119311+AkhilAkhilesh@users.noreply.github.com>
2026-01-20 10:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	s=s.trim()
+	let newstr=""
+	for(let c of s){
+		if(c!==" ") newstr+=c;
+		else return newstr;
+	}
+	return newstr;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
